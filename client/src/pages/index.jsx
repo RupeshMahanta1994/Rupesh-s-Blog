bd20bd04a8d8e9ac3a01c134037f17cf4c5f6867
rupesh-hub <mahanta.rupesh704@gmail.com>
2025-03-14 10:10:13 +0530

    removed the repo

--- client/src/pages/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as About } from "./About";
-export { default as Projects } from "./Projects";
-export { default as SignUp } from "./SignUp";
-export { default as SignIn } from "./SignIn";
-export { default as Dashboard } from "./Dashboard";
